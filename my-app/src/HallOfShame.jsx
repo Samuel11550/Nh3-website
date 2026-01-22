cb1ac99a4fd5405f0da03a4eaa9fc847d0c67a24
Samuel11550 <samm.teljevik@gmail.com>
2026-01-22 22:07:06 +0100

    fixat med hos

--- my-app/src/HallOfShame.jsx
+++ my-app/src/HallOfShame.jsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import axios from "axios";
-import { addEntry, deleteEntry } from "./hallOfShameSlice";
+import { addEntry, deleteEntry, getList } from "./hallOfShameSlice";
 import { useNavigate } from 'react-router-dom';
 import './HallOfShame.css';
 
